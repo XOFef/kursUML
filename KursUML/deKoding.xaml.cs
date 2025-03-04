1fc6a1232f0b23deb74a14309423f8e7e37097ec
Keqing <146825224+XOFef@users.noreply.github.com>
2025-03-04 23:22:29 +0300

    add coding

---
+++ KursUML/deKoding.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KursUML
+{
+    /// <summary>
+    /// Логика взаимодействия для deKoding.xaml
+    /// </summary>
+    public partial class deKoding : Window
+    {
+        public deKoding()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            tBox.Text = string.Empty;
+        }
+
+        private void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(Char.IsDigit(e.Text, 0) || e.Text == "."))
+            {
+                e.Handled = true; // Запрещаем ввод, если символ не является цифрой или точкой
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            history history = new history();
+            history.Show();
+            this.Close();
+        }
+
+
+    }
+}
